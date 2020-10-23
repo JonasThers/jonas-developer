22506bae797bc493d9ae8374104b5c344219856a
JonasThers <jonasthers1992@gmail.com>
2020-10-23 18:47:53 +0200

    Json

---
+++ src/json/Education.js
@@ -0,0 +1,14 @@
+const Educationsdata = [
+  {
+    degree: "Msc in Digital Design & Communication",
+    school: "IT-University of Copenhagen",
+    electives: "Software Development",
+  },
+  {
+    degree: "Bsc in Information Science",
+    school: "University of Copenhagen",
+    electives: "Communication & IT",
+  },
+];
+
+export default Educationsdata;
---
+++ src/json/Work.js
@@ -0,0 +1,46 @@
+const Workdata = [
+  {
+    title: "Frontend Developer",
+    workplace: "Lamia",
+    start: "August 2019",
+    end: "Now",
+    technologies: [
+      "Magento 2",
+      "PHP",
+      "Drupal 8",
+      "Vue",
+      "Contentful",
+      "Sass",
+    ],
+  },
+  {
+    title: "Student Developer",
+    workplace: "C&C Travel",
+    start: "October 2018",
+    end: "March 2019",
+    technologies: ["PHP", "MySQL", "CSS", "Drupal 7 & 8"],
+  },
+  {
+    title: "Frontend Teacher",
+    workplace: "Coding Pirates",
+    start: "September 2018",
+    end: "December 2018",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Volunteer Developer",
+    workplace: "Action Child Aid",
+    start: "February 2018",
+    end: "October 2018",
+    technologies: ["Wordpress", "PHP", "CSS"],
+  },
+  {
+    title: "Web & Communication Student",
+    workplace: "Management of Culture and Leisure at Copenhagen Municipality",
+    start: "October 2017",
+    end: "September 2018",
+    technologies: ["Drupal 7", "Google My Business"],
+  },
+];
+
+export default Workdata;
